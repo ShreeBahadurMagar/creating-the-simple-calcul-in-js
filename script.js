5e85a375ea7a5d39c16a5fcc1422a705f0756577
Shree Bahadur Magar <112608714+ShreeBahadurMagar@users.noreply.github.com>
2022-11-02 04:07:41 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,23 @@
+function add(num1, num2) {
+  return num1 + num2;
+}
+function sub(num1, num2) {
+  return num1 - num2;
+}
+function mul(num1, num2) {
+  return num1 * num2;
+}
+function div(num1, num2) {
+  return num1 / num2;
+}
+function mod(num1, num2) {
+  return num1 % num2;
+}
+
+function Calculator(num1, num2, operator) {
+  return operator(num1, num2)
+}
+
+
+
+// Debugger is the tool which shows the process of codes in sequence order. 
